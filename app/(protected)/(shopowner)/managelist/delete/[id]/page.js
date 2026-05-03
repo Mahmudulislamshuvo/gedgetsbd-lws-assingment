cf9b94ba30b85e72f2c6c9499028f85d3681332e
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-05-04 01:16:34 +0600

    feat: add DeleteProductModal component for product deletion confirmation

---
+++ app/(protected)/(shopowner)/managelist/delete/[id]/page.js
@@ -0,0 +1,8 @@
+import DeleteProductModal from "@/components/managelist/DeleteProductModal";
+import React from "react";
+
+const DeleteSingleProductModal = () => {
+  return <DeleteProductModal />;
+};
+
+export default DeleteSingleProductModal;
